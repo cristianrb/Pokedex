2f805cfc1d4c0cfd4fdb3a2429978501f004db05
Cristian Ruiz <cristianrb235@gmail.com>
2021-12-30 13:38:51 +0100

    Fetch Poke API

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -1,10 +1,37 @@
-import { Box } from '@material-ui/core';
-import React from 'react';
+import { Box, CircularProgress } from '@material-ui/core';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config';
 
 function Pokedex() {
+
+    const [pokemonData, setPokemonData] = useState(null)
+
+    useEffect(() => {
+        axios.get(POKEMON_API_URL + "?limit=800")
+             .then((response) => {
+                 if (response.status >= 200 && response.status < 300) {
+                     const { results } = response.data
+                     let newPokemonData = []
+                     results.forEach((pokemon, index) => {
+                         index++
+                         let pokemonObject = {
+                             id: index,
+                             url: IMAGE_API_URL + index + ".png",
+                             name: pokemon.name
+                         }
+                         newPokemonData.push(pokemonObject)
+                     });
+                     setPokemonData(newPokemonData)
+                 }
+             })
+    }, [])
+
     return (
         <Box>
-            
+            {pokemonData ? pokemonData.map(pokemon => {
+                return <h1 key={pokemon.id}>{pokemon.name}</h1>
+            }) : <CircularProgress style={{marginTop: 100}}   />}
         </Box>
         
     );
